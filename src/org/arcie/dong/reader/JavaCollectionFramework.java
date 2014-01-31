273f04a7c9884f75b6989f7eb17dfdcd83ac7610
AntonLiu <liuchundong2008@gmail.com>
2014-02-01 01:42:41 +0800

    Signed-off-by: AntonLiu <liuchundong2008@gmail.com>

--- src/org/arcie/dong/reader/JavaCollectionFramework.java
+++ src/org/arcie/dong/reader/JavaCollectionFramework.java
@@ -1,6 +1,8 @@
 package org.arcie.dong.reader;
 
+import java.util.AbstractCollection;
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
@@ -9,9 +11,10 @@ import java.util.Vector;
 /**
  * @author 刘春东
  * @version 
+ * @param <E>
  * @time 2014年1月30日下午4:30:37
  */
-public class JavaCollectionFramework {
+public class JavaCollectionFramework<E> {
 	/**
 	 * 容器类Collection
 	 */
@@ -20,4 +23,8 @@ public class JavaCollectionFramework {
 	Vector<?> vector = null;
 	HashMap<?, ?> hm;
 	Hashtable<?, ?> ht;
+	
+	AbstractCollection<E> sc;
+	EnumSet<?> es;
+	
 }
