df2184d24ada74b4a3491e88085c279b408adf05
Liuchundong <liuchundong2008@gmail.com>
2016-12-07 21:21:47 +0800

    1、add httpclient

---
+++ com.github.liuxboy.httpclient/src/main/java/com/github/liuxboy/httpclient/controller/controller.java
@@ -0,0 +1,12 @@
+package com.github.liuxboy.httpclient.controller;
+
+/**
+ * Package: com.github.liuxboy.httpclient <br>
+ * Author: liuchundong <br>
+ * Date: 2016/12/7 <br>
+ * Time: 15:07 <br>
+ * Desc:
+ */
+public class controller {
+
+}
